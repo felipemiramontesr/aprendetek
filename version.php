2169b86e38da0a1d49f55c394b91358cb199f97f
felipemiramontesr <felipemiramontesr@gmail.com>
2026-03-22 20:59:10 -0600

    V0.0.3_Force_Theme_Installation_Detection

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026032200;
+$plugin->version = 2026032201;
 $plugin->requires = 2023042400; // Moodle 4.2+ base
 $plugin->component = 'theme_aprendetek';
 $plugin->dependencies = ['theme_boost' => 2023042400];
