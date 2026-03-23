9b25f866d2dc2abfbb9a87033aed80d301458da0
felipemiramontesr <felipemiramontesr@gmail.com>
2026-03-22 20:37:55 -0600

    V0.0.2_Dynamic_Branding_And_Settings

--- lib.php
+++ lib.php
@@ -4,14 +4,27 @@ defined('MOODLE_INTERNAL') || die();
 function theme_aprendetek_get_main_scss_content($theme_config) {
     $scss = '';
     
-    // Obtener el valor de brandcolor de la configuración y asignarlo a una variable de SASS
+    // Brand color injection
     $brandcolor = get_config('theme_aprendetek', 'brandcolor');
     if (!empty($brandcolor)) {
         $scss .= '$brand-color: ' . $brandcolor . ';' . "\n";
+        $scss .= '$primary: $brand-color;' . "\n";
     }
     
-    // Concatenar el contenido de Boost
+    // Inclusion of theme_boost SCSS
     $scss .= theme_boost_get_main_scss_content($theme_config);
     
     return $scss;
+}
+
+/**
+ * Serves the logo from the theme settings.
+ */
+function theme_aprendetek_get_logo_url() {
+    global $OUTPUT;
+    $logo = get_config('theme_aprendetek', 'logo');
+    if ($logo) {
+        return $OUTPUT->get_logo_url(null, 150);
+    }
+    return false;
 }
