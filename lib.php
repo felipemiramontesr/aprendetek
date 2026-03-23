2744005e67559654494243f89664e5701be968fa
felipemiramontesr <felipemiramontesr@gmail.com>
2026-03-22 21:11:54 -0600

    V0.0.5_Restore_Full_Modern_Functionality

--- lib.php
+++ lib.php
@@ -3,5 +3,28 @@ defined('MOODLE_INTERNAL') || die();
 
 // Minimal lib.php for installation detection.
 function theme_aprendetek_get_main_scss_content($theme_config) {
-    return theme_boost_get_main_scss_content($theme_config);
+    $scss = '';
+    $brandcolor = get_config('theme_aprendetek', 'brandcolor');
+    if (!empty($brandcolor)) {
+        $scss .= '$brand-color: ' . $brandcolor . ';' . "\n";
+        $scss .= '$primary: $brand-color;' . "\n";
+    }
+    $scss .= theme_boost_get_main_scss_content($theme_config);
+    return $scss;
+}
+
+/**
+ * Serves the logo from the theme settings.
+ */
+function theme_aprendetek_get_logo_url() {
+    global $OUTPUT;
+    return $OUTPUT->get_logo_url(null, 150);
+}
+
+/**
+ * Serves the compact logo (isotipo) from the theme settings.
+ */
+function theme_aprendetek_get_compact_logo_url() {
+    global $OUTPUT;
+    return $OUTPUT->get_compact_logo_url(null, 70);
 }
